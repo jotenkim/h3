80cd4fd7373b1e64bce64aa9190f1599a53ac73a
Kim <wertyy@gmail.com>
2017-04-26 07:19:30 +0000

    bash script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get -y install puppet git
 git clone https://github.com/jotenkim/h3.git
 cd h3	
-bash apply.sh
+sudo puppet apply --modulepath puppet/modules/ -e 'class {apache:}'

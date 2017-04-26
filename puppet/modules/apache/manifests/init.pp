3a371c5484d45d64182b2bb64e49af907f945a3f
Kim <wertyy@gmail.com>
2017-04-26 07:01:23 +0000

    Added puppet module

---
+++ puppet/modules/apache/manifests/init.pp
@@ -0,0 +1,40 @@
+class apache {
+
+	package {'apache2':
+		ensure => 'installed',
+	}
+	
+	package {'php7.0':
+		ensure => 'installed',
+	}
+	
+	package {'libapache2-mod-php7.0':
+		ensure => 'installed',
+	}
+
+	file {'/etc/apache2/mods-available/php7.0.conf':
+		content => template('apache/php7.0.conf.erb'),	
+	}
+
+	file {'/home/xubuntu/public_html':
+		ensure => 'directory',
+	}
+
+	file {'/home/xubuntu/public_html/index.php':
+		content => '<?php print 2+2 ?>',
+	}
+	
+	exec {'a2enmod': 
+		command => '/usr/sbin/a2enmod userdir',
+		notify => Service["apache2"],
+		require => Package["apache2"],
+	}
+	
+	service {'apache2':
+		ensure => 'running',
+		enable => 'true',
+		require => Package["apache2"],
+	}
+
+
+}

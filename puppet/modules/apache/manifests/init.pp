b17c480b8523dc5f2fdf63f126e1d09dcbae5d17
Kim <wertyy@gmail.com>
2017-04-26 08:11:10 +0000

    addedd lines to init

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -4,15 +4,22 @@ class apache {
 		ensure => 'installed',
 		allowcdrom => 'true',
 	}
+
+	service {'apache2':
+                ensure => 'running',
+                enable => 'true',
+                require => Package["apache2"],
+        }
+
 	
 	package {'php7.0':
 		ensure => 'installed',
-		 allowcdrom => 'true',		
+		allowcdrom => 'true',		
 	}
 	
 	package {'libapache2-mod-php7.0':
 		ensure => 'installed',
-		 allowcdrom => 'true',
+		allowcdrom => 'true',
 	}
 
 	file {'/etc/apache2/mods-available/php7.0.conf':
@@ -32,12 +39,22 @@ class apache {
 		notify => Service["apache2"],
 		require => Package["apache2"],
 	}
-	
-	service {'apache2':
-		ensure => 'running',
-		enable => 'true',
-		require => Package["apache2"],
-	}
 
+	file { '/etc/apache2/mods-enabled/userdir.load':
+                ensure => 'link',
+                target => '/etc/apache2/mods-available/userdir.load',
+                notify => Service["apache2"],
+                require => Package["apache2"],
+        }
+
+        file { '/etc/apache2/mods-enabled/userdir.conf':
+                ensure => 'link',
+                target => '/etc/apache2/mods-available/userdir.conf',
+                notify => Service["apache2"],
+                require => Package["apache2"],
+        }
+	
 
 }
+
+
